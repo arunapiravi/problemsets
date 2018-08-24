515ae1cb937d331d6fa129c5b92352e6eb4d2104
Aruna Piravi <aruna@couchbase.com>
2018-08-23 18:52:53 -0700

    Adding my solution to move_zeroes

--- move_zeroes.py
+++ move_zeroes.py
@@ -16,3 +16,26 @@ def moveZeroes(self, nums):
         if nums[i] != 0:
             nums[i], nums[zero] = nums[zero], nums[i]
             zero += 1
+
+def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        j = 1
+        while j < len(nums):
+            if nums[i] == 0 and nums[j] != 0:
+                # swap
+                print "swapping {0}, {1}".format(nums[i], nums[j])
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+            elif nums[i] == 0 and nums[j] == 0:
+                # just increment j to swap on next non-zero
+                 j += 1
+            else:
+                # no swap needed
+                i += 1
+                j += 1
+               
